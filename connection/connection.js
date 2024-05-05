8bd03170ef4b6754b24cf9100b4961319fb0b5ac
NotSnowWhite <gflatch@att.net>
2024-05-05 13:13:21 -0500

    added tables and started prompt. updated connection so that my creds are private. worked on creating a pool

--- connection/connection.js
+++ connection/connection.js
@@ -1,20 +1,10 @@
 require('dotenv').config();
 
-const { Client } = require('pg');
-const client = new Client({
+const { Pool } = require('pg');
+const pool = new Pool({
     database: process.env.DATABASE_NAME,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
 });
 
-(async () => {
-    try {
-        await client.connect()
-        console.log('Connection to the database was successful!')
-    } catch (error) {
-        console.error(error)
-    } finally {
-        await client.end()
-        console.log('Connection to the database has ended.')
-    }
-})();
+module.exports = pool;
--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const inquirer = require('inquirer');
-const connection = require('./connection/connection');
+const pool = require('./connection/connection');
 
 const app = express();
-const PORT = process.env.PORT || PORT;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+function init() {
+    inquirer.prompt([
+        {
+            name: '',
+            type: 'input',
+            message: ,
+        }
+    ])
+}
+
+(async () => {
+    try {
+        await pool.connect();
+        console.log('Connection to the database was successful!')
+    } catch (error) {
+        console.error(error)
+    }
+})();
 
 app.listen(PORT, () => {
     console.log(`Listening on port http://www.localhost:${PORT}`)
