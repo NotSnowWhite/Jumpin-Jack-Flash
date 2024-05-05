262e1cdac3ec5fdc56dde81d0d7ff5a1591633a7
NotSnowWhite <gflatch@att.net>
2024-05-05 15:53:28 -0500

    small updates - added seeds template and switch statement to handle answers

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const pool = require('./connection/connection');
 
 async function init() {
     try {
-        inquirer.prompt([
+        await inquirer.prompt([
             {
                 type: 'list',
                 name: 'option',
@@ -17,20 +17,53 @@ async function init() {
                     'Add an employee',
                     'Update an employee role',
                 ]
-            },
+            }
+        ]);
+        switch (response.option) {
+            case 'View all departments':
+            pool.query(`SELECT * FROM departments`, (err, res) => {
+                if (err) {
+                    console.error(err)
+                }
+                    console.table(res)
+            })
+                break;
+            case 'View all roles':
 
-        ]); 
+                break;
+            case 'View all employees':
+
+                break;
+            case 'Add a department':
+
+                break;
+            case 'Add a role':
+
+                break;
+            case 'Add an employee':
+
+                break;
+                case 'Update an employee role':
+
+                break;
+            default:
+                console.log('Invalid option. Try again.');
+                break;
+        }
     } catch (err) {
         console.error("An error has occurred", err)
     }
-}
+};
 
 (async () => {
     try {
         await pool.connect();
         console.log('Connection to the database was successful!')
-        init()
+        await init()
     } catch (error) {
         console.error(error)
+    } finally {
+        await pool.end()
+        console.log('The connection has been severed. Goodbye.')
     }
 })();
